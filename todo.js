3193bd2b6d87c33d0e90cd39e9c7f833a73083e5
Mr-Courteous <105926598+Mr-Courteous@users.noreply.github.com>
2024-06-11 09:37:01 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -32,7 +32,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use(cors({
-    origin: ' http://localhost:3000', // Replace with your frontend domain
+    origin: ['https://todo-frontend-ten-phi.vercel.app', ' http://localhost:3000'], // Replace with your frontend domain
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true, // Allow credentials (cookies)
 
